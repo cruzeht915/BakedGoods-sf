fe7b6aca6ac6081ee0a755a5171fff061c0de32c
Elmer Cruz'git config --global user.name Elmer <elmercruzgalvan2019@gmail.com>
2025-04-15 23:50:53 -0500

    Troubleshooting db connection (was a node version error *cry*)

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env.local' })
 import mongoose from "mongoose"
 
 let isConnected = false
@@ -6,9 +8,13 @@ export const connectToDB = async () => {
     if (isConnected) return
 
     try{
-        await mongoose.connect(process.env.MONGODB_URI!, {
-            dbName: "baked_goods"
-        })
+        const mongoURI = process.env.MONGODB_URI
+        if (!mongoURI) {
+            throw new Error('❌ MONGODB_URI not defined in .env.local')
+          }
+        await mongoose.connect(mongoURI!, {
+            dbName: 'baked_goods',
+          })
         isConnected = true
         console.log("✅ MongoDB connected")
     } catch(err) {
