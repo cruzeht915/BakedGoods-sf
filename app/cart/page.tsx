7bfaec576fef8911f22b01c0bd4048796dc846ce
Elmer Cruz'git config --global user.name Elmer <elmercruzgalvan2019@gmail.com>
2025-04-19 22:08:26 -0500

    Implemented cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { useCart } from "@/context/CartContext"
+
+export default function CartPage() {
+    const {cart, removeFromCart, clearCart} = useCart()
+
+    const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0)
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Your Cart</h1>
+            {cart.length === 0 ? (
+                <p>Your cart is empty</p>
+            ) : (
+                <>
+                    <ul className="space-y-4">
+                        {cart.map((item) => (
+                            <li 
+                                key={item.productId}
+                                className="flex justify-between border-b pb-2"
+                            >
+                                <span>
+                                    {item.name} x {item.quantity}
+                                </span>
+                                <span>${(item.price * item.quantity).toFixed(2)}</span>
+                                <button 
+                                    className="text-red-500 ml-4"
+                                    onClick={() => removeFromCart(item.productId)}
+                                >
+                                    Remove
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className="mt-6 text-right font-semibold">
+                        Total: ${total.toFixed(2)}
+                    </div>
+
+                    <div className="flex gap-4 mt-4 justify-end">
+                        <button
+                            onClick={clearCart}
+                            className="px-4 py-2 bg-gray-300 text-black rounded"
+                        >
+                            Clear Cart
+                        </button>
+                        <button
+                            onClick={() => alert('Checkout soon!')}
+                            className="px-4 py-2 bg-green-600 text-white rounded"
+                        >
+                            Checkout
+                        </button>
+                    </div>
+                </>
+            )}
+
+        </div>
+    )
+}
