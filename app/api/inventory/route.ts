e4e2b4f1958e96684bda671dcc3c35b35a0fbcb1
Elmer Cruz'git config --global user.name Elmer <elmercruzgalvan2019@gmail.com>
2025-04-22 16:09:52 -0500

    Bug in inventoory api route

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -2,10 +2,9 @@ import { connectToDB } from "@/lib/mongodb";
 import Inventory from "@/models/Inventory";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function Post(req: NextRequest) {
+export async function POST(req: NextRequest) {
     const {date} = await req.json()
     await connectToDB()
-
     const inventory = await Inventory.find({date}).populate('product')
     return NextResponse.json(inventory)
 }
