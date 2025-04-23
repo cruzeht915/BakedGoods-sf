23721a0f47423b1cb6ee0c0a5f170e5eb493a7cb
Elmer Cruz'git config --global user.name Elmer <elmercruzgalvan2019@gmail.com>
2025-04-23 16:11:30 -0500

    Misc changes for inventory build bug

--- app/api/inventory/route.ts
+++ app/api/inventory/route.ts
@@ -1,10 +1,12 @@
 import { connectToDB } from "@/lib/mongodb";
 import Inventory from "@/models/Inventory";
 import { NextRequest, NextResponse } from "next/server";
+import { ProductType } from "@/models/Product";
 
 export async function POST(req: NextRequest) {
     const {date} = await req.json()
     await connectToDB()
     const inventory = await Inventory.find({date}).populate('product')
+    console.log('📦 populated inventory:', JSON.stringify(inventory, null, 2))
     return NextResponse.json(inventory)
 }
